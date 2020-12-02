ed412f4aa476482ca6b39a5e7ab41f02298a8799
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-12-02 12:56:34 +0900

    Initialize Repository

---
+++ app/javascript/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app id="app"> <!-- 追加 -->
+    <v-app-bar color='purple' dark app>
+      <v-toolbar-title>TwitchJpTrend</v-toolbar-title>
+    </v-app-bar>
+    <v-footer color='purple' dark app>
+      TwitchJpTrend
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
