e2e3bdf2807960cba478e121fc2386025bb2943b
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-12-03 13:59:50 +0900

    add: twitchのクライアントIDとaccess_tokenを追加

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module TwitchJpTrend
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    
+
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
   end
